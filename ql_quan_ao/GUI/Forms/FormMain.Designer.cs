3f89715d089a42bc9cbabf660d2c792bf7bf4f3e
Bùi Quang Huy <quanghuybui530@gmail.com>
2026-05-23 11:15:53 +0700

    Hoan thanh giao dien menu

---
+++ ql_quan_ao/GUI/Forms/FormMain.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace ql_quan_ao
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelHeader = new System.Windows.Forms.Panel();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.lblQuanlyquanao = new System.Windows.Forms.Label();
+            this.btnBanHang = new System.Windows.Forms.Button();
+            this.btnLichSu = new System.Windows.Forms.Button();
+            this.btnSanPham = new System.Windows.Forms.Button();
+            this.btnKhoHang = new System.Windows.Forms.Button();
+            this.btnKhachHang = new System.Windows.Forms.Button();
+            this.btnThongKe = new System.Windows.Forms.Button();
+            this.lblAdmin = new System.Windows.Forms.Label();
+            this.panelContent = new System.Windows.Forms.Panel();
+            this.panelHeader.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelHeader
+            // 
+            this.panelHeader.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.panelHeader.Controls.Add(this.tableLayoutPanel1);
+            this.panelHeader.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelHeader.Location = new System.Drawing.Point(0, 0);
+            this.panelHeader.Name = "panelHeader";
+            this.panelHeader.Size = new System.Drawing.Size(1212, 67);
+            this.panelHeader.TabIndex = 0;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 8;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tableLayoutPanel1.Controls.Add(this.lblQuanlyquanao, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.btnBanHang, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.btnLichSu, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.btnSanPham, 3, 0);
+            this.tableLayoutPanel1.Controls.Add(this.btnKhoHang, 4, 0);
+            this.tableLayoutPanel1.Controls.Add(this.btnKhachHang, 5, 0);
+            this.tableLayoutPanel1.Controls.Add(this.btnThongKe, 6, 0);
+            this.tableLayoutPanel1.Controls.Add(this.lblAdmin, 7, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1212, 67);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // lblQuanlyquanao
+            // 
+            this.lblQuanlyquanao.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblQuanlyquanao.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblQuanlyquanao.Location = new System.Drawing.Point(3, 0);
+            this.lblQuanlyquanao.Name = "lblQuanlyquanao";
+            this.lblQuanlyquanao.Size = new System.Drawing.Size(145, 67);
+            this.lblQuanlyquanao.TabIndex = 0;
+            this.lblQuanlyquanao.Text = "CỬA HÀNG QUẦN ÁO";
+            this.lblQuanlyquanao.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnBanHang
+            // 
+            this.btnBanHang.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnBanHang.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBanHang.Location = new System.Drawing.Point(154, 3);
+            this.btnBanHang.Name = "btnBanHang";
+            this.btnBanHang.Size = new System.Drawing.Size(145, 61);
+            this.btnBanHang.TabIndex = 1;
+            this.btnBanHang.Text = "BÁN HÀNG";
+            this.btnBanHang.UseVisualStyleBackColor = true;
+            this.btnBanHang.Click += new System.EventHandler(this.btnBanHang_Click);
+            // 
+            // btnLichSu
+            // 
+            this.btnLichSu.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnLichSu.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLichSu.Location = new System.Drawing.Point(305, 3);
+            this.btnLichSu.Name = "btnLichSu";
+            this.btnLichSu.Size = new System.Drawing.Size(145, 61);
+            this.btnLichSu.TabIndex = 2;
+            this.btnLichSu.Text = "LỊCH SỬ";
+            this.btnLichSu.UseVisualStyleBackColor = true;
+            this.btnLichSu.Click += new System.EventHandler(this.btnLichSu_Click);
+            // 
+            // btnSanPham
+            // 
+            this.btnSanPham.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnSanPham.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSanPham.Location = new System.Drawing.Point(456, 3);
+            this.btnSanPham.Name = "btnSanPham";
+            this.btnSanPham.Size = new System.Drawing.Size(145, 61);
+            this.btnSanPham.TabIndex = 3;
+            this.btnSanPham.Text = "SẢN PHẨM";
+            this.btnSanPham.UseVisualStyleBackColor = true;
+            this.btnSanPham.Click += new System.EventHandler(this.btnSanPham_Click);
+            // 
+            // btnKhoHang
+            // 
+            this.btnKhoHang.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnKhoHang.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnKhoHang.Location = new System.Drawing.Point(607, 3);
+            this.btnKhoHang.Name = "btnKhoHang";
+            this.btnKhoHang.Size = new System.Drawing.Size(145, 61);
+            this.btnKhoHang.TabIndex = 4;
+            this.btnKhoHang.Text = "KHO HÀNG";
+            this.btnKhoHang.UseVisualStyleBackColor = true;
+            this.btnKhoHang.Click += new System.EventHandler(this.btnKhoHang_Click);
+            // 
+            // btnKhachHang
+            // 
+            this.btnKhachHang.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnKhachHang.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnKhachHang.Location = new System.Drawing.Point(758, 3);
+            this.btnKhachHang.Name = "btnKhachHang";
+            this.btnKhachHang.Size = new System.Drawing.Size(145, 61);
+            this.btnKhachHang.TabIndex = 5;
+            this.btnKhachHang.Text = "KHÁCH HÀNG";
+            this.btnKhachHang.UseVisualStyleBackColor = true;
+            this.btnKhachHang.Click += new System.EventHandler(this.btnKhachHang_Click);
+            // 
+            // btnThongKe
+            // 
+            this.btnThongKe.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnThongKe.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnThongKe.Location = new System.Drawing.Point(909, 3);
+            this.btnThongKe.Name = "btnThongKe";
+            this.btnThongKe.Size = new System.Drawing.Size(145, 61);
+            this.btnThongKe.TabIndex = 6;
+            this.btnThongKe.Text = "THỐNG KÊ";
+            this.btnThongKe.UseVisualStyleBackColor = true;
+            this.btnThongKe.Click += new System.EventHandler(this.btnThongKe_Click);
+            // 
+            // lblAdmin
+            // 
+            this.lblAdmin.AutoSize = true;
+            this.lblAdmin.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblAdmin.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAdmin.Location = new System.Drawing.Point(1060, 0);
+            this.lblAdmin.Name = "lblAdmin";
+            this.lblAdmin.Size = new System.Drawing.Size(149, 67);
+            this.lblAdmin.TabIndex = 7;
+            this.lblAdmin.Text = "ADMIN";
+            this.lblAdmin.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblAdmin.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // panelContent
+            // 
+            this.panelContent.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelContent.Location = new System.Drawing.Point(0, 67);
+            this.panelContent.Name = "panelContent";
+            this.panelContent.Size = new System.Drawing.Size(1212, 524);
+            this.panelContent.TabIndex = 1;
+            // 
+            // FormMain
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1212, 591);
+            this.Controls.Add(this.panelContent);
+            this.Controls.Add(this.panelHeader);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "FormMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "HỆ THỐNG QUẢN LÝ CỬA HÀNG QUẦN ÁO";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.FormMain_Load);
+            this.panelHeader.ResumeLayout(false);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelHeader;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label lblQuanlyquanao;
+        private System.Windows.Forms.Button btnBanHang;
+        private System.Windows.Forms.Button btnLichSu;
+        private System.Windows.Forms.Button btnSanPham;
+        private System.Windows.Forms.Button btnKhoHang;
+        private System.Windows.Forms.Button btnKhachHang;
+        private System.Windows.Forms.Button btnThongKe;
+        private System.Windows.Forms.Label lblAdmin;
+        private System.Windows.Forms.Panel panelContent;
+    }
+}
+
