f71e5ef727c81a38d6d05d0acc09abd1e115d607
Phan Nhat Phong <phannhatphong06@gmail.com>
2026-05-26 03:50:38 +0700

    Hoan thanh chuc nang bao cao thong ke

---
+++ ql_quan_ao/GUI/Forms/Form_ChiTietHoaDon.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace ql_quan_ao.GUI.Forms
+{
+    partial class Form_ChiTietHoaDon
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvDanhSach = new System.Windows.Forms.DataGridView();
+            this.colMaHD = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colMaSP = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colSoLuong = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colDonGia = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colThanhTien = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDanhSach)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvDanhSach
+            // 
+            this.dgvDanhSach.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvDanhSach.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDanhSach.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.colMaHD,
+            this.colMaSP,
+            this.colSoLuong,
+            this.colDonGia,
+            this.colThanhTien});
+            this.dgvDanhSach.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvDanhSach.Location = new System.Drawing.Point(0, 0);
+            this.dgvDanhSach.Name = "dgvDanhSach";
+            this.dgvDanhSach.RowHeadersWidth = 62;
+            this.dgvDanhSach.RowTemplate.Height = 28;
+            this.dgvDanhSach.Size = new System.Drawing.Size(800, 450);
+            this.dgvDanhSach.TabIndex = 0;
+            // 
+            // colMaHD
+            // 
+            this.colMaHD.DataPropertyName = "MaHD";
+            this.colMaHD.HeaderText = "MaHD";
+            this.colMaHD.MinimumWidth = 8;
+            this.colMaHD.Name = "colMaHD";
+            // 
+            // colMaSP
+            // 
+            this.colMaSP.DataPropertyName = "MaSP";
+            this.colMaSP.HeaderText = "MaSP";
+            this.colMaSP.MinimumWidth = 8;
+            this.colMaSP.Name = "colMaSP";
+            // 
+            // colSoLuong
+            // 
+            this.colSoLuong.DataPropertyName = "SoLuong";
+            this.colSoLuong.HeaderText = "SoLuong";
+            this.colSoLuong.MinimumWidth = 8;
+            this.colSoLuong.Name = "colSoLuong";
+            // 
+            // colDonGia
+            // 
+            this.colDonGia.DataPropertyName = "DonGia";
+            this.colDonGia.HeaderText = "DonGia";
+            this.colDonGia.MinimumWidth = 8;
+            this.colDonGia.Name = "colDonGia";
+            // 
+            // colThanhTien
+            // 
+            this.colThanhTien.DataPropertyName = "ThanhTien";
+            this.colThanhTien.HeaderText = "ThanhTien";
+            this.colThanhTien.MinimumWidth = 8;
+            this.colThanhTien.Name = "colThanhTien";
+            // 
+            // Form_ChiTietHoaDon
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dgvDanhSach);
+            this.Name = "Form_ChiTietHoaDon";
+            this.Text = "Form_ChiTietHoaDon";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDanhSach)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvDanhSach;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colMaHD;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colMaSP;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colSoLuong;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colDonGia;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colThanhTien;
+    }
+}
---
+++ ql_quan_ao/GUI/Forms/Form_ChiTietHoaDon.cs
@@ -0,0 +1,46 @@
+﻿using QuanLyCuaHang.DAL;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ql_quan_ao.GUI.Forms
+{
+    public partial class Form_ChiTietHoaDon : Form
+    {
+        string _maHD;
+
+        // Constructor nhận mã hóa đơn
+        public Form_ChiTietHoaDon(string maHD)
+        {
+            InitializeComponent();
+            _maHD = maHD;
+        }
+
+        private void Form_ChiTietHoaDon_Load(object sender, EventArgs e)
+        {
+            // Kiểm tra biến _maHD có giá trị hay không
+            if (string.IsNullOrEmpty(_maHD)) return;
+
+            // Câu lệnh SQL "trần trụi" để kiểm tra
+            string sql = "SELECT * FROM ChiTietHoaDon WHERE MaHD = '" + _maHD + "'";
+
+            // Gọi thực thi
+            DataTable dt = DataProvider.Instance.ExecuteQuery(sql);
+
+            // BÁO CÁO KẾT QUẢ ĐỂ CHÚNG TA BIẾT LỖI Ở ĐÂU
+            MessageBox.Show("Số dòng lấy được từ CSDL: " + dt.Rows.Count);
+
+            if (dt.Rows.Count > 0)
+            {
+                dgvDanhSach.DataSource = dt;
+            }
+        }
+    }
+}
+
